f897b07738e8a944b9823e8affe562e6ff5e98c2
Mariana S Oliveira <marianasoli621@gmail.com>
2025-02-21 14:40:56 -0300

    fixed tailwind

--- src/app/criarconta/page.tsx
+++ src/app/criarconta/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 import CriarConta from "../_components/criarConta";
 
-export default function criarConta() {
+export default function CriarContaPage() {
   return (
-    <main className="flex-justify-center flex-1">
+    <main className="flex justify-center flex-1">
       <div className="flex items-center">
         <img
-            src="/logosbg.png"
-            alt="Logo escola do cerrado"
-            className="h-[664px] w-[718px] object-contain mr-20px"
-          />
+          src="/logosbg.png"
+          alt="Logo escola do cerrado"
+          className="h-[664px] w-[718px] object-contain mr-5"
+        />
         <CriarConta />
       </div> 
     </main>
