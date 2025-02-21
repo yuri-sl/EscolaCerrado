65737c971c059c926afae0c6b8cb25e08283593f
Mariana S Oliveira <marianasoli621@gmail.com>
2025-02-21 13:58:32 -0300

    mudança na pagina

--- src/app/criarconta/page.tsx
+++ src/app/criarconta/page.tsx
@@ -1,10 +1,17 @@
 "use client";
 import CriarConta from "../_components/criarConta";
 
-export default function Home() {
+export default function criarConta() {
   return (
-    <main>
-      <CriarConta />
+    <main className="flex-justify-center flex-1">
+      <div className="flex items-center">
+        <img
+            src="/logosbg.png"
+            alt="Logo escola do cerrado"
+            className="h-[664px] w-[718px] object-contain mr-20px"
+          />
+        <CriarConta />
+      </div> 
     </main>
   );
 }
