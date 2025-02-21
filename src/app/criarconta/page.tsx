05bdfd0a457cff734a6c1a9d7c760cf26698550b
Mariana S Oliveira <marianasoli621@gmail.com>
2025-02-21 14:58:50 -0300

    mais correções tela de login

--- src/app/criarconta/page.tsx
+++ src/app/criarconta/page.tsx
@@ -3,12 +3,12 @@ import CriarConta from "../_components/criarConta";
 
 export default function CriarContaPage() {
   return (
-    <main className="flex justify-center flex-1">
-      <div className="flex items-center">
+    <main className="flex justify-center flex-1 mt-11 mr-20">
+      <div className="flex items-center ">
         <img
           src="/logosbg.png"
           alt="Logo escola do cerrado"
-          className="h-[664px] w-[718px] object-contain mr-5"
+          className="h-[764px] w-[818px] object-contain mr-20"
         />
         <CriarConta />
       </div> 
