573883df36d4bb8a8677b1f38b06120d0b815b05
Mariana S Oliveira <marianasoli621@gmail.com>
2025-02-21 15:09:43 -0300

    + correções criarConta

--- src/app/_components/criarConta.tsx
+++ src/app/_components/criarConta.tsx
@@ -45,77 +45,77 @@ export default function CriarConta() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col items-center bg-white border-[14px] border-[#f5f4e8] rounded-[98px] p-8 shadow-lg w-[679px] h-[960px]">
-      <h1 className="text-[64px] font-bold text-[#3e300f] mb-4 text-center">Fazer Cadastro</h1>
+    <form onSubmit={handleSubmit} className="flex flex-col items-center bg-Branco border-[14px] border-Creme rounded-[98px] p-8 shadow-lg w-[679px] h-[960px]">
+      <h1 className="text-[64px] font-bold text-MarromEscuro mb-4 text-center">Fazer Cadastro</h1>
       
       <img src={imagem} alt="Foto de perfil" className="w-[176px] h-[157px] rounded-full mb-4 mt-4 object-cover" />
       
-      <label className="bg-[#bfbe50] text-white py-2 px-4 rounded cursor-pointer mb-4 hover:bg-[#f5f261] hover:text-orange-600">
+      <label className="bg-Verde text-white py-2 px-4 rounded-[51px] cursor-pointer mb-4 hover:bg-Amarelo hover:text-orange-600">
         Inserir imagem
         <input type="file" className="hidden" onChange={handleImageUpload} />
       </label>
 
       <div className="w-full mb-2">
-        <label className="block text-[#3e300f] font-bold text-[24px]">Nome:</label>
+        <label className="block text-MarromEscuro font-bold text-[24px]">Nome:</label>
         <input
           type="text"
           name="nome"
           placeholder="Insira seu nome"
           value={nome}
           onChange={(e) => setNome(e.target.value)}
-          className="w-full bg-[#f5f4e8] p-2 rounded-[50px] text-[20px]"
+          className="w-full bg-Creme p-2 rounded-[50px] text-[20px]"
         />
       </div>
       
       <div className="w-full mb-2">
-        <label className="block text-[#3e300f] font-bold text-[24px]">Email:</label>
+        <label className="block text-MarromEscuro font-bold text-[24px]">Email:</label>
         <input
           type="email"
           name="email"
           placeholder="Insira seu email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className="w-full bg-[#f5f4e8] p-2 rounded-[50px] text-[20px]"
+          className="w-full bg-Creme p-2 rounded-[50px] text-[20px]"
         />
       </div>
 
       <div className="w-full mb-2">
-        <label className="block text-[#3e300f] font-bold text-[24px]">Cargo:</label>
+        <label className="block text-MarromEscuro font-bold text-[24px]">Cargo:</label>
         <input
           type="text"
           name="cargo"
           placeholder="Defina seu cargo"
           value={cargo}
           onChange={(e) => setCargo(e.target.value)}
-          className="w-full bg-[#f5f4e8] p-2 rounded-[50px] text-[20px]"
+          className="w-full bg-Creme p-2 rounded-[50px] text-[20px]"
         />
       </div>
 
       <div className="w-full mb-4">
-        <label className="block text-[#3e300f] font-bold text-[24px]">Senha:</label>
+        <label className="block text-MarromEscuro font-bold text-[24px]">Senha:</label>
         <input
           type="password"
           name="senha"
           placeholder="Defina sua senha"
           value={senha}
           onChange={(e) => setSenha(e.target.value)}
-          className="w-full bg-[#f5f4e8] p-2 rounded-[50px] text-[20px]"
+          className="w-full bg-Creme p-2 rounded-[50px] text-[20px]"
         />
       </div>
 
       {erro && <p className="text-red-600 font-bold">{erro}</p>}
 
-      <button type="submit" className="bg-[#bfbe50] px-6 py-2 rounded-[51px] text-white font-bold text-[24px] w-40 h-12 hover:bg-[#f5f261] hover:text-orange-600">
+      <button type="submit" className="bg-Verde px-6 py-2 rounded-[51px] text-white font-bold text-[24px] w-40 h-12 hover:bg-Amarelo hover:text-orange-600">
         Criar
       </button>
 
       {sucesso && (
-        <div className="bg-green-500 text-white text-[20px] font-bold px-4 py-2 rounded mt-4">
+        <div className="bg-Verde text-white text-[20px] font-bold px-4 py-2 rounded mt-4">
           Conta criada com sucesso!
         </div>
       )}
 
-      <a href="#" onClick={() => router.push("/login")} className="text-[#a6551c] font-bold text-[20px] mt-4 hover:underline hover:text-orange-600">
+      <a href="#" onClick={() => router.push("/login")} className="text-Marrom font-bold text-[20px] mt-4 hover:underline hover:text-orange-600">
         Já possui uma conta? Faça login
       </a>
     </form>
