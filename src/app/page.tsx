cb611382344d3f9f3ee1af4e380259bb3f5799ae
isjr04 <iisaacjunior2004@gmail.com>
2025-02-25 20:18:57 -0300

    Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./_components/navbar";
 import NossaEquipe from "./_components/NossaEquipe";
 import NossosCases from "./_components/NossosCases";
+import Footer from "./_components/Footer";
 import "../styles/globals.css";
 
 export default function Homepage() {
@@ -12,6 +13,9 @@ export default function Homepage() {
       </section>
       <section id="equipe">
         <NossaEquipe />
+      </section>
+      <section>
+        <Footer />
       </section>
     </main>
   );
