acf49bc5f43bc5dce330f4799e5807c20cf791c3
isjr04 <iisaacjunior2004@gmail.com>
2025-02-25 23:03:09 -0300

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@ import Navbar from "./_components/navbar";
 import NossaEquipe from "./_components/NossaEquipe";
 import NossosCases from "./_components/NossosCases";
 import Footer from "./_components/Footer";
+import HeroSection from "./_components/HeroSection";
 import "../styles/globals.css";
 
 export default function Homepage() {
   return (
     <main className="pt-[137px]">
       <Navbar />
+      <section>
+        <HeroSection />
+      </section>
       <section id="cases">
         <NossosCases />
       </section>
